849097671a014ea62fa130dc7e622aecb69eeab2
koldoxo <luis.torres-cisneros@outlook.com>
2025-12-02 20:36:46 +0100

    add opencv for development

--- app/Main.cpp
+++ app/Main.cpp
@@ -5,9 +5,13 @@
 
 #include "CircleDetector.h"
 
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
 int main(void)
 {
-    std::cout << "Hi Circle Detection" << std::endl;
+    std::cout << "Circle Detection..." << std::endl;
 
     std::ifstream ifs("C:\\Git\\koldoxo\\ZTask\\app\\config.json");
     nlohmann::json file = nlohmann::json::parse(ifs);
@@ -15,6 +19,10 @@ int main(void)
     std::cout << file["inputfile"]  << std::endl;
     std::cout << file["outputfile"] << std::endl;
   
+    cv::Mat image = cv::imread(file["inputfile"]);
+
+    cv::imshow("source image", image);
+    cv::waitKey();
 
     return EXIT_SUCCESS;
 }
