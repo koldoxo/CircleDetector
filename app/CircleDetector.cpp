f19583def5c63832329657753df3ea1135b9172c
koldoxo <luis.torres-cisneros@outlook.com>
2026-01-05 15:23:44 +0100

    add recent changes to repo

--- app/CircleDetector.cpp
+++ app/CircleDetector.cpp
@@ -16,15 +16,9 @@
 // [Local] 
 #include "CircleDetectorLib.h"
 
-cv::Mat src_gray;
-int thresh = 100;
-cv::RNG rng(12345);
-
 void curvature(cv::Mat& src, cv::Mat& dst, double sigma, std::filesystem::path outputpath, std::string stem);
 
 void save_image(cv::Mat& im, std::filesystem::path outputpath, std::string stem, std::string sufix);
-
-void thresh_callback(int, void*);
 
 int main(void)
 {
@@ -67,49 +61,31 @@ int main(void)
   
     // read data
     cv::Mat source = cv::imread(jsonfile["inputfile"], cv::IMREAD_GRAYSCALE);
-    cv::Mat source_s;
-    cv::resize(source, source_s, cv::Size(int(source.cols / 3), int(source.rows / 3) ) );
 
     cv::Mat binary; 
     {
-        cv::threshold(source, binary, 50, 255, cv::THRESH_BINARY);
+        cv::threshold(source, binary, 50.f, 1.f, cv::THRESH_BINARY);
         std::filesystem::path outputbinary = outputpath / (inputfile.stem().string() + "_bin.png");
         cv::imwrite(outputbinary.string(), binary);
     }
     
-    auto sigma = 0.5;
-    cv::Mat temp;
 
-    int size = std::floor(2 * sigma * 2 + 1); // 95% gauge
-    {
-        cv::GaussianBlur(binary, temp, cv::Size(size, size), sigma, sigma, cv::BORDER_CONSTANT);
-        save_image(temp, outputpath, inputfile.stem().string(), "_blurr");
-    }
+    cv::Mat destination = cv::Mat::zeros(source.size(), CV_32F); // CV_16U 
+    
+    // compute curvatue
+    curvature(binary, destination, 255, outputpath, inputfile.stem().string());
 
-    std::vector<std::vector<cv::Point> > contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(temp, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-    cv::Mat drawing = cv::Mat::zeros(temp.size(), CV_8UC3);
-    for (size_t i = 0; i < contours.size(); i++)
-    {
-        cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-        cv::drawContours(drawing, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0);
-    }
-    
-    save_image(drawing, outputpath, inputfile.stem().string(), "_contours");
-    
-    //cv::imshow("Contours", drawing);
+    // save image
+    save_image(binary, outputpath, inputfile.stem().string(), "_bin");
+
+    //vector<vector<cv::Point> > contours;
+    //vector<Vec4i> hierarchy;
+    //findContours( binary, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE );
+
+
+    // show result
+    //cv::imshow("curvature", destination);
     //cv::waitKey();
-    /*
-    const char* source_window = "Source";
-    cv::namedWindow(source_window);
-    cv::imshow(source_window, source);
-
-    const int max_thresh = 255;
-    cv::createTrackbar("Canny thresh:", source_window, &thresh, max_thresh, thresh_callback);
-    thresh_callback(0, 0);
-    cv::waitKey();
-    */
 
     return EXIT_SUCCESS;
 }
@@ -129,21 +105,7 @@ void curvature(cv::Mat& src, cv::Mat& dst, double sigma, std::filesystem::path o
         cv::GaussianBlur(src, temp, cv::Size(size, size), sigma, sigma, cv::BORDER_CONSTANT);
         save_image(temp, outputpath, stem, "_blurr");
     }
-   
-}
+    
 
-void thresh_callback(int, void*)
-{
-    cv::Mat canny_output;
-    //Canny(src_gray, canny_output, thresh, thresh * 2);
-    std::vector<std::vector<cv::Point> > contours;
-    std::vector<cv::Vec4i> hierarchy;
-    cv::findContours(src_gray, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
-    cv::Mat drawing = cv::Mat::zeros(canny_output.size(), CV_8UC3);
-    for (size_t i = 0; i < contours.size(); i++)
-    {
-        cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-        cv::drawContours(drawing, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0);
-    }
-    cv::imshow("Contours", drawing);
+   
 }
