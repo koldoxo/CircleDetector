fe583d81668bcdf0290a630d13e09935fec1dd2a
koldoxo <luis.torres-cisneros@outlook.com>
2026-01-05 12:10:00 -0600

    add newest implemention for contour determination

--- app/CircleDetector.cpp
+++ app/CircleDetector.cpp
@@ -18,15 +18,19 @@
 
 cv::RNG rng(12345);
 
-void curvature(cv::Mat& src, cv::Mat& dst, double sigma, std::filesystem::path outputpath, std::string stem);
-
 void save_image(cv::Mat& im, std::filesystem::path outputpath, std::string stem, std::string sufix);
+
+void unpack_parameters(nlohmann::json inputjson, std::map<std::string, std::float_t>& params);
+
+void check_parameters(const std::map<std::string, std::float_t>& params);
+
+void pre_processing(cv::Mat& in, cv::Mat& out, std::map<std::string, std::float_t>& params);
 
 int main(void)
 {
     std::cout << "Circle Detection..." << std::endl;
 
-    // check congi file
+    // check config file
     std::filesystem::path inputconfig("C:\\Git\\koldoxo\\CircleDetector\\app\\config.json");
 
     if (!std::filesystem::exists(inputconfig))
@@ -39,7 +43,7 @@ int main(void)
     nlohmann::json jsonfile = nlohmann::json::parse(ifs);
 
     // check input image
-    std::string inputfilestr = jsonfile["inputfile"];
+    std::string inputfilestr = jsonfile["input_file"];
     std::filesystem::path inputfile(inputfilestr);
    
     if (!std::filesystem::exists(inputfile))
@@ -47,11 +51,11 @@ int main(void)
         throw std::invalid_argument("Input file entry does not exist");
     }
 
-    std::cout << "Reading in... " << inputfile << std::endl;
+    std::cout << "Reading from... " << inputfile << std::endl;
 
     //check output folder
 
-    std::string outputpathstr = jsonfile["outputpath"];
+    std::string outputpathstr = jsonfile["output_path"];
     std::filesystem::path outputpath(outputpathstr);
 
     if (!std::filesystem::is_directory(outputpath))
@@ -61,38 +65,38 @@ int main(void)
 
     std::cout << "Writing to... " << outputpath << std::endl;
   
-    // read data
-    cv::Mat source = cv::imread(jsonfile["inputfile"], cv::IMREAD_GRAYSCALE);
+    // unpack parameters
+    std::map<std::string, std::float_t> parameters;
+    unpack_parameters(jsonfile, parameters);
 
-    cv::Mat binary; 
-    {
-        cv::threshold(source, binary, 50.f, 255.f, cv::THRESH_BINARY);
-        std::filesystem::path outputbinary = outputpath / (inputfile.stem().string() + "_bin.png");
-        cv::imwrite(outputbinary.string(), binary);
-    }
+    // check parameters
+    check_parameters(parameters);
+
+
+    // read data
+    cv::Mat source = cv::imread(jsonfile["input_file"], cv::IMREAD_GRAYSCALE);
     
-    // cv::Mat destination = cv::Mat::zeros(source.size(), CV_32F); // CV_16U 
-    
-    // compute curvatue
-    // curvature(binary, destination, 255, outputpath, inputfile.stem().string());
 
     // save image
-    save_image(binary, outputpath, inputfile.stem().string(), "_bin");
+    // save_image(binary, outputpath, inputfile.stem().string(), "_bin");
+    //std::float_t sigma = 1.f;
+    //cv::Mat blurr;
+    //int size = std::floor(2 * sigma * 2 + 1); // 95% gauge
+    //{
+    //    cv::GaussianBlur(binary, blurr, cv::Size(size, size), sigma, sigma, cv::BORDER_CONSTANT);
+    //    save_image(blurr, outputpath, inputfile.stem().string(), "_blurr");
+    //}
+    
+
+    cv::Mat proc;
+
+    pre_processing(source, proc, parameters);
 
     std::vector<std::vector<cv::Point> > contours;
     std::vector<cv::Vec4i> hierarchy;
-    
-    std::float_t sigma = 1.f;
-    cv::Mat blurr;
-    int size = std::floor(2 * sigma * 2 + 1); // 95% gauge
-    {
-        cv::GaussianBlur(binary, blurr, cv::Size(size, size), sigma, sigma, cv::BORDER_CONSTANT);
-        save_image(blurr, outputpath, inputfile.stem().string(), "_blurr");
-    }
-    
-    cv::findContours(blurr, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE );
+    cv::findContours(proc, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE );
 
-    cv::Mat drawing = cv::Mat::zeros(binary.size(), CV_8UC3);
+    cv::Mat drawing = cv::Mat::zeros(source.size(), CV_8UC3);
     for (size_t i = 0; i < contours.size(); i++)
     {
         cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
@@ -101,26 +105,64 @@ int main(void)
 
     save_image(drawing, outputpath, inputfile.stem().string(), "_contours");
 
-    // show result
+    //show result
     //cv::imshow("curvature", destination);
     //cv::waitKey();
 
     return EXIT_SUCCESS;
 }
 
+void pre_processing(cv::Mat& in, cv::Mat& out, std::map<std::string, std::float_t>& params)
+{
+    
+    cv::Mat bin;
+    std::float_t t = params.find("threshold")->second;
+    {
+        cv::threshold(in, bin, t, 255.f, cv::THRESH_BINARY);
+    }
+    
+    //cv::Mat blurr;
+    std::float_t s = params.find("sigma")->second;
+    int size = std::floor(2 * s * 2 + 1); // 95% gauge
+    {
+        cv::GaussianBlur(bin, out, cv::Size(size, size), s, s, cv::BORDER_CONSTANT);
+    }
+}
+
+
+void unpack_parameters(nlohmann::json jsonfile, std::map<std::string, std::float_t>& params)
+{
+    std::string thresholdstr = jsonfile["threshold"];
+    std::float_t threshold = std::atof(thresholdstr.c_str());
+    params["threshold"] = threshold;
+
+    std::string sigmastr = jsonfile["sigma"];
+    std::float_t sigma = std::atof(sigmastr.c_str());
+    params["sigma"] = sigma;
+
+    std::string min_lengthstr = jsonfile["min_length"];
+    std::float_t min_length = std::atof(min_lengthstr.c_str());
+    params["min_length"] = min_length;
+
+    std::string window_sizestr = jsonfile["window_size"];
+    std::float_t window_size = std::atof(window_sizestr.c_str());
+    params["window_size"] = window_size;
+
+}
+
+void check_parameters(const std::map<std::string, std::float_t>& params)
+{
+    for (const auto& [key, value] : params)
+    {
+        if (value < 0.f)
+        {
+            throw std::invalid_argument("At least one parameter is lesser than zero");
+        }
+    }
+}
+
 void save_image(cv::Mat& im, std::filesystem::path outputpath, std::string stem, std::string sufix)
 {
     std::filesystem::path outputfile = outputpath / (stem + sufix + ".png");
     cv::imwrite(outputfile.string(), im);
-}
-
-void curvature(cv::Mat& src, cv::Mat& dst, double sigma, std::filesystem::path outputpath, std::string stem)
-{
-    cv::Mat temp;
-
-    int size = std::floor( 2*sigma*2 + 1 ); // 95% gauge
-    {
-        cv::GaussianBlur(src, temp, cv::Size(size, size), sigma, sigma, cv::BORDER_CONSTANT);
-        save_image(temp, outputpath, stem, "_blurr");
-    }
 }
